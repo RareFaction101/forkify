b5ff6687e37a9e7b92ce9b2048fb69c588656fd8
RareFaction101 <xherox14@gmail.com>
2021-03-12 13:25:41 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6904e8fa-813f-4ab9-b104-3ed310f24474';
+export const MODAL_CLOSE_SEC = 2.5;
